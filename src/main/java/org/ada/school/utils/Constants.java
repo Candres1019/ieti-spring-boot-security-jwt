d80e4df1e2857c6d60284c0d985df7360a125fa5
Andres Calderon <andres.calderon@payu.com>
2021-09-03 14:06:59 -0500

    Creation of Constants final class

---
+++ src/main/java/org/ada/school/utils/Constants.java
@@ -0,0 +1,13 @@
+package org.ada.school.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constants {
+
+	public static final String CLAIMS_ROLES_KEY = "Hola";
+
+	public static final int TOKEN_DURATION_MINUTES = 24;
+
+}
