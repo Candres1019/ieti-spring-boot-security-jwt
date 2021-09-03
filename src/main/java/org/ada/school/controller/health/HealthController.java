9a380a54e095195aa23c6d8f68788d207831a800
Andres Calderon <andres.calderon@payu.com>
2021-09-03 09:15:28 -0500

    Formatting using intellij custom formatting tool and addition of static string message.

--- src/main/java/org/ada/school/controller/health/HealthController.java
+++ src/main/java/org/ada/school/controller/health/HealthController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping( "v1/health" )
-public class HealthController
-{
+@RequestMapping("/v1/health")
+public class HealthController {
 
-    @GetMapping
-    public String all()
-    {
-        return "API Working OK!";
-    }
+    private static final String HEALTH_MESSAGE = "API Working OK!";
+
+	@GetMapping
+	public String all() {
+
+		return HEALTH_MESSAGE;
+	}
+
 }
