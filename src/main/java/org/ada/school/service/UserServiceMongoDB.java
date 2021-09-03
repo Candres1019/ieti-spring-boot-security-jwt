f81c18b18c60e239387e2653595f6a9be4feb599
Andres Calderon <andres.calderon@payu.com>
2021-09-03 09:06:50 -0500

    Addition of use of the lombok project, and formatting using custom formatting tool from intelliJ.

--- src/main/java/org/ada/school/service/UserServiceMongoDB.java
+++ src/main/java/org/ada/school/service/UserServiceMongoDB.java
@@ -1,89 +1,76 @@
 package org.ada.school.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
 import org.ada.school.controller.user.UserDto;
 import org.ada.school.exception.UserNotFoundException;
 import org.ada.school.repository.UserRepository;
 import org.ada.school.repository.document.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Optional;
 
-@Service
-public class UserServiceMongoDB
-    implements UserService
-{
+@Component("userServiceMongoDB")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class UserServiceMongoDB implements UserService {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    public UserServiceMongoDB( @Autowired UserRepository userRepository )
-    {
-        this.userRepository = userRepository;
-    }
+	@Override
+	public User create(UserDto userDto) {
 
-    @Override
-    public User create( UserDto userDto )
-    {
-        return userRepository.save( new User( userDto ) );
-    }
+		return userRepository.save(new User(userDto));
+	}
 
-    @Override
-    public User findById( String id )
-    {
-        Optional<User> optionalUser = userRepository.findById( id );
-        if ( optionalUser.isPresent() )
-        {
-            return optionalUser.get();
-        }
-        else
-        {
-            throw new UserNotFoundException();
-        }
-    }
+	@Override
+	public User findById(String id) {
 
-    @Override
-    public User findByEmail( String email )
-        throws UserNotFoundException
-    {
-        Optional<User> optionalUser = userRepository.findByEmail( email );
-        if ( optionalUser.isPresent() )
-        {
-            return optionalUser.get();
-        }
-        else
-        {
-            throw new UserNotFoundException();
-        }
-    }
+		Optional<User> optionalUser = userRepository.findById(id);
+		if (optionalUser.isPresent()) {
+			return optionalUser.get();
+		} else {
+			throw new UserNotFoundException();
+		}
+	}
 
-    @Override
-    public List<User> all()
-    {
-        return userRepository.findAll();
-    }
+	@Override
+	public User findByEmail(String email) throws UserNotFoundException {
 
-    @Override
-    public boolean deleteById( String id )
-    {
-        if ( userRepository.existsById( id ) )
-        {
-            userRepository.deleteById( id );
-            return true;
-        }
-        return false;
-    }
+		Optional<User> optionalUser = userRepository.findByEmail(email);
+		if (optionalUser.isPresent()) {
+			return optionalUser.get();
+		} else {
+			throw new UserNotFoundException();
+		}
+	}
 
-    @Override
-    public User update( UserDto userDto, String id )
-    {
-        if ( userRepository.findById( id ).isPresent() )
-        {
-            User user = userRepository.findById( id ).get();
-            user.update( userDto );
-            userRepository.save( user );
-            return user;
-        }
-        return null;
-    }
+	@Override
+	public List<User> all() {
+
+		return userRepository.findAll();
+	}
+
+	@Override
+	public boolean deleteById(String id) {
+
+		if (userRepository.existsById(id)) {
+			userRepository.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public User update(UserDto userDto, String id) {
+
+		if (userRepository.findById(id).isPresent()) {
+			User user = userRepository.findById(id).get();
+			user.update(userDto);
+			userRepository.save(user);
+			return user;
+		}
+		return null;
+	}
 }
