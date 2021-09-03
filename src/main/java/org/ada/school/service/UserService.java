969ab892adb70dd5d9778618839f5d2483a52d8b
Andres Calderon <andres.calderon@payu.com>
2021-09-03 09:04:02 -0500

    Code formatting using custom intelliJ formatting tool.

--- src/main/java/org/ada/school/service/UserService.java
+++ src/main/java/org/ada/school/service/UserService.java
@@ -1,24 +1,22 @@
 package org.ada.school.service;
 
+import java.util.List;
+
 import org.ada.school.controller.user.UserDto;
 import org.ada.school.exception.UserNotFoundException;
 import org.ada.school.repository.document.User;
 
-import java.util.List;
+public interface UserService {
 
-public interface UserService
-{
-    User create( UserDto userDto );
+	User create(UserDto userDto);
 
-    User findById( String id )
-        throws UserNotFoundException;
+	User findById(String id) throws UserNotFoundException;
 
-    User findByEmail( String email )
-        throws UserNotFoundException;
+	User findByEmail(String email) throws UserNotFoundException;
 
-    List<User> all();
+	List<User> all();
 
-    boolean deleteById( String id );
+	boolean deleteById(String id);
 
-    User update( UserDto userDto, String id );
+	User update(UserDto userDto, String id);
 }
