9bb8851342e252791f3eb709341af39cd1334a18
Andres Calderon <andres.calderon@payu.com>
2021-09-06 10:24:59 -0500

    Modification on Constants due to new information found

--- src/main/java/org/ada/school/utils/Constants.java
+++ src/main/java/org/ada/school/utils/Constants.java
@@ -6,8 +6,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Constants {
 
-	public static final String CLAIMS_ROLES_KEY = "Hola";
+	public static final String CLAIMS_ROLES_KEY = "ada_roles";
 
-	public static final int TOKEN_DURATION_MINUTES = 24;
+	public static final int TOKEN_DURATION_MINUTES = 240;
 
+	public static final int ADMIN_TOKEN_DURATION_MINUTES = 10;
+
+	public static final String COOKIE_NAME = "ada-JWT";
+
+	public static final String ADMIN_ROLE = "ADMIN";
+
+	public static final String USER_ROLE = "USER";
 }
