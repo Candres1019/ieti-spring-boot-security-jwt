2060e604a9c8242bf68146dd6f62a22fec811c68
Andres Calderon <andres.calderon@payu.com>
2021-09-06 10:31:06 -0500

    The user endpoint available for any user, addition of lombok and jwt filter

--- src/main/java/org/ada/school/config/SecurityConfiguration.java
+++ src/main/java/org/ada/school/config/SecurityConfiguration.java
@@ -1,23 +1,31 @@
 package org.ada.school.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	private final JwtRequestFilter jwtRequestFilter;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.cors().and().csrf().disable()
+		http.addFilterBefore(jwtRequestFilter, BasicAuthenticationFilter.class)
+			.cors().and().csrf().disable()
 			.authorizeRequests()
 			.antMatchers(HttpMethod.GET, "/v1/health").permitAll()
 			.antMatchers(HttpMethod.POST, "/v1/auth").permitAll()
+			.antMatchers(HttpMethod.POST, "/v1/user").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
